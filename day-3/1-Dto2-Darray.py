a814fc346a1f6833131ed443197a853d588d7cb3
--unset <roywamaitha2001@gmail.com>
2022-01-13 19:47:11 +0300

    Merge remote-tracking branch 'origin/numpy_attributes' into numpy_attributes

--- day-3/1-Dto2-Darray.py
+++ day-3/1-Dto2-Darray.py
@@ -1,8 +1,7 @@
 # converting from one dimension array to two-dimension array.
 
 import numpy as np
-
-x = np.array([x for x in range(16)]
-final = x.reshape((4,4)
+x = np.array([x for x in range(9)])
+final = x.reshape((3,3))
 print(final)
 
